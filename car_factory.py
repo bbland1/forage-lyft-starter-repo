2d92a62af9840d7faa9cb57476824e1112d0969b
bbland1 <104288486+bbland1@users.noreply.github.com>
2023-10-18 16:07:51 -0400

    fix: added the engine logic to the other CarFactory methods

--- car_factory.py
+++ car_factory.py
@@ -1,26 +1,37 @@
 from datetime import date
 from car import Car
 from engine.capulet_engine import CapuletEngine
-from battery.battery import NubbinBattery
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+from battery.spindler_battery import SpindlerBattery
+from battery.nubbin_battery import NubbinBattery
 
 
 
 class CarFactory:
     def create_calliope(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
         engine_type = CapuletEngine(last_service_date, current_mileage, last_service_mileage)
-        battery_type = NubbinBattery(last_service_date, current_date)
+        battery_type = SpindlerBattery(last_service_date, current_date)
         return Car(engine_type, battery_type)
 
     def create_glissade(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
-        return Car("Willoughby", "Spindler", current_date, last_service_date, current_mileage, last_service_mileage)
+        engine_type = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery_type = SpindlerBattery(current_date, last_service_date)
+        return Car(engine_type, battery_type)
 
     def create_palindrome(self, current_date: date, last_service_date: date, warning_indicator_on: bool) -> Car:
-        return Car("Sternman", "Spindler", current_date, last_service_date, warning_indicator_on)
+        engine_type = SternmanEngine(warning_indicator_on)
+        battery_type = SpindlerBattery(current_date, last_service_date)
+        return Car(engine_type, battery_type)
 
     def create_rorschach(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
-        return Car("Willoughby", "Nubbin", current_date, last_service_date, current_mileage, last_service_mileage)
+        engine_type = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery_type = NubbinBattery(current_date, last_service_date)
+        return Car(engine_type, battery_type)
 
     def  create_thovex(self, current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
-        return Car("Capulet", "Nubbin", current_date, last_service_date, current_mileage, last_service_mileage)
+        engine_type = CapuletEngine(current_mileage, last_service_mileage)
+        battery_type = NubbinBattery(current_date, last_service_date)
+        return Car(engine_type, battery_type)
 
     
--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -2,7 +2,7 @@ from engine import Engine
 
 
 class CapuletEngine(Engine):
-    def __init__(self, current_mileage: int, last_service_mileage: int):
+    def __init__(self, current_mileage, last_service_mileage):
         self._current_mileage = current_mileage
         self._last_service_mileage = last_service_mileage
 
