b6ae2a5edadbcd4acdc6456453c6144307757a62
bbland1 <104288486+bbland1@users.noreply.github.com>
2023-10-19 02:29:07 -0400

    feat: adding failing tests to car factory to start adding tires to the factory

--- tests/test_car_factory.py
+++ tests/test_car_factory.py
@@ -7,6 +7,8 @@ from engine.sternman_engine import SternmanEngine
 from engine.willoughby_engine import WilloughbyEngine
 from battery.nubbin_battery import NubbinBattery
 from battery.spindler_battery import SpindlerBattery
+from tire.carrigan_tire import CarriganTire
+from tire.octoprime_tire import OctoprimeTire
 
 factory = CarFactory()
 
@@ -18,6 +20,7 @@ class TestCarFactory(unittest.TestCase):
             month=current_date.month - 4).replace(day=current_date.day + 7)
         current_mileage = 23000
         last_service_mileage = 12450
+        tire_wear_sensor = [0.9, 0.1, 0.2, 0.5]
 
         calliope = factory.create_calliope(
             current_date, last_service_date, current_mileage, last_service_mileage)
@@ -32,6 +35,9 @@ class TestCarFactory(unittest.TestCase):
                         last_service_date)
         self.assertEqual(calliope._battery._current_date, current_date)
 
+        self.assertIsInstance(calliope._tire, CarriganTire)
+        self.assertEqual(calliope._tire._tire_wear_sensors, tire_wear_sensor)
+
         self.assertTrue(calliope.needs_service())
 
     def test_creating_glissade(self):
@@ -40,6 +46,7 @@ class TestCarFactory(unittest.TestCase):
             month=current_date.month - 4).replace(day=current_date.day + 7)
         current_mileage = 23000
         last_service_mileage = 12450
+        tire_wear_sensor = [0.9, 0.1, 0.2, 0.5]
 
         glissade = factory.create_glissade(
             current_date, last_service_date, current_mileage, last_service_mileage)
@@ -54,6 +61,10 @@ class TestCarFactory(unittest.TestCase):
                         last_service_date)
         self.assertEqual(glissade._battery._current_date, current_date)
 
+
+        self.assertIsInstance(glissade._tire, OctoprimeTire)
+        self.assertEqual(glissade._tire._tire_wear_sensors, tire_wear_sensor)
+
         self.assertTrue(glissade.needs_service())
 
     def test_creating_palindrome(self):
@@ -61,6 +72,7 @@ class TestCarFactory(unittest.TestCase):
         last_service_date = current_date.replace(year=current_date.year - 3).replace(
             month=current_date.month - 4).replace(day=current_date.day + 7)
         warning_indicator_on = False
+        tire_wear_sensor = [0.9, 0.1, 0.2, 0.5]
 
         palindrome = factory.create_palindrome(
             current_date, last_service_date, warning_indicator_on)
@@ -73,6 +85,10 @@ class TestCarFactory(unittest.TestCase):
                         last_service_date)
         self.assertEqual(palindrome._battery._current_date, current_date)
 
+
+        self.assertIsInstance(palindrome._tire, CarriganTire)
+        self.assertEqual(palindrome._tire._tire_wear_sensors, tire_wear_sensor)
+
         self.assertTrue(palindrome.needs_service())
 
     def test_creating_rorschach(self):
@@ -81,6 +97,7 @@ class TestCarFactory(unittest.TestCase):
             month=current_date.month - 4).replace(day=current_date.day + 7)
         current_mileage = 23000
         last_service_mileage = 12450
+        tire_wear_sensor = [0.9, 0.1, 0.2, 0.5]
 
         rorschach = factory.create_rorschach(
             current_date, last_service_date, current_mileage, last_service_mileage)
@@ -95,6 +112,9 @@ class TestCarFactory(unittest.TestCase):
                         last_service_date)
         self.assertEqual(rorschach._battery._current_date, current_date)
 
+        self.assertIsInstance(rorschach._tire, OctoprimeTire)
+        self.assertEqual(rorschach._tire._tire_wear_sensors, tire_wear_sensor)
+
         self.assertFalse(rorschach.needs_service())
 
     def test_creating_thovex(self):
@@ -103,6 +123,7 @@ class TestCarFactory(unittest.TestCase):
             month=current_date.month - 4).replace(day=current_date.day + 7)
         current_mileage = 23000
         last_service_mileage = 12450
+        tire_wear_sensor = [0.9, 0.1, 0.2, 0.5]
 
         thovex = factory.create_thovex(
             current_date, last_service_date, current_mileage, last_service_mileage)
@@ -116,5 +137,9 @@ class TestCarFactory(unittest.TestCase):
         self.assertEqual(thovex._battery._last_service_date,
                         last_service_date)
         self.assertEqual(thovex._battery._current_date, current_date)
+
+
+        self.assertIsInstance(thovex._tire, CarriganTire)
+        self.assertEqual(thovex._tire._tire_wear_sensors, tire_wear_sensor)
 
         self.assertFalse(thovex.needs_service())
