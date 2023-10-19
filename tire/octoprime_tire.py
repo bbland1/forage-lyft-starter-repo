be2d838979cc0cbde0293dcc51ae46845ce6b15c
bbland1 <104288486+bbland1@users.noreply.github.com>
2023-10-19 02:07:14 -0400

    feat: adding octoprime tire logic

---
+++ tire/octoprime_tire.py
@@ -0,0 +1,13 @@
+from tire.tire import Tire
+
+class OctoprimeTire(Tire):
+    def __init__(self, tire_wear_sensors: list[float]):
+        self._tire_wear_sensor = tire_wear_sensors
+
+    def needs_service(self) -> bool:
+        if all(0 <= wear_value <= 1 for wear_value in self._tire_wear_sensor):
+            wear_values_sum = sum(self._tire_wear_sensor)
+
+            return wear_values_sum >= 3
+        else:
+            raise ValueError("Check the tire sensor, one of the values was outside of the possible range.")
