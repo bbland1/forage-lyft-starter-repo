027eb111c2c5ccb07bc5350834dbf017b793f852
bbland1 <104288486+bbland1@users.noreply.github.com>
2023-10-19 02:05:46 -0400

    feat: added tests for octoprime tire

--- tests/test_tires.py
+++ tests/test_tires.py
@@ -1,6 +1,7 @@
 import unittest
 
 from tire.carrigan_tire import CarriganTire
+from tire.octoprime_tire import OctoprimeTire
 
 
 class TestCarriganTire(unittest.TestCase):
@@ -15,6 +16,20 @@ class TestCarriganTire(unittest.TestCase):
     def test_value_issue_error(self):
         with self.assertRaises(ValueError):
             tire = CarriganTire(tire_wear_sensors=[1,0, 2,0.5])
+            tire.needs_service()
+
+class TestOctoprimeTire(unittest.TestCase):
+    def test_octoprime__should_be_serviced(self):
+        tire = OctoprimeTire(tire_wear_sensors=[1, 0.9, 0.9, 0.4])
+        self.assertTrue(tire.needs_service())
+
+    def test_octoprime__should_not_be_serviced(self):
+        tire = OctoprimeTire(tire_wear_sensors=[0.1, 0.1, 0.1, 0.1])
+        self.assertFalse(tire.needs_service())
+
+    def test_value_issue_error(self):
+        with self.assertRaises(ValueError):
+            tire = OctoprimeTire(tire_wear_sensors=[1,0, 2,0.5])
             tire.needs_service()
 
 if __name__ == '__main__':
